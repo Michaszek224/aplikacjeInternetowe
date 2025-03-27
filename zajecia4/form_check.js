284178dc8f7b76c98478c236f594f849c4c91eda
Michal <punkomichal@gmail.com>
2025-03-27 18:12:33 +0100

    na zadaniu 18

--- zajecia4/form_check.js
+++ zajecia4/form_check.js
@@ -5,14 +5,6 @@ function isEmpty(inputField) {
     return false;
 }
 
-function validate(formularz) {
-    checkString(formularz.elements["f_imie"].value, "Wpisz imię!");
-    checkString(formularz.elements["f_nazwisko"].value, "Wpisz nazwisko!");
-    checkString(formularz.elements["f_kod"].value, "Wpisz kod pocztowy!");
-    checkString(formularz.elements["f_ulica"].value, "Wpisz ulicę!");
-    checkString(formularz.elements["f_miasto"].value, "Wpisz miasto!");
-}
-
 function isWhiteSpaceOrEmpty(str) {
     return /^[\s\t\r\n]*$/.test(str);
 }
@@ -23,6 +15,59 @@ function checkString(ciagZnakow, wiadomosc) {
         return false;
     }
     return true;
+}
+
+function checkEmail(str) {
+    let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+    if (email.test(str))
+        return true;
+    else {
+        alert("Podaj właściwy e-mail");
+        return false;
+    }
+}
+
+function checkStringAndFocus(obj, msg) {
+    let str = obj.value;
+    let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+    if (isWhiteSpaceOrEmpty(str)) {
+        document.getElementById(errorFieldName).innerHTML = msg;
+        obj.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function checkEmailAndFocus(obj) {
+    let str = obj.value;
+    let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+    if (checkEmail(str)) {
+        document.getElementById(errorFieldName).innerHTML = "";
+        return true;
+    }
+    else {
+        document.getElementById(errorFieldName).innerHTML = "Podaj właściwy e-mail";
+        obj.focus();
+        return false;
+    }
+}
+
+function validate(formularz) {
+    // checkString(formularz.elements["f_imie"].value, "Wpisz imię!");
+    // checkString(formularz.elements["f_nazwisko"].value, "Wpisz nazwisko!");
+    // checkString(formularz.elements["f_kod"].value, "Wpisz kod pocztowy!");
+    // checkString(formularz.elements["f_ulica"].value, "Wpisz ulicę!");
+    // checkString(formularz.elements["f_miasto"].value, "Wpisz miasto!");
+    // checkEmail(formularz.elements["f_email"].value);
+    checkStringAndFocus(formularz.elements["f_imie"], "Wpisz imię!");
+    checkStringAndFocus(formularz.elements["f_nazwisko"], "Wpisz nazwisko!");
+    checkStringAndFocus(formularz.elements["f_kod"], "Wpisz kod pocztowy!");
+    checkStringAndFocus(formularz.elements["f_ulica"], "Wpisz ulicę!");
+    checkStringAndFocus(formularz.elements["f_miasto"], "Wpisz miasto!");
+    checkEmailAndFocus(formularz.elements["f_email"]);
+
 }
 
 console.log("form_check.js załadowany");
