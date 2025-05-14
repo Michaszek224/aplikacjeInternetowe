b50668acbc02a4844bbe4131c1b63deb726fb4e9
michaszek <punkomichal@gmail.com>
2025-05-14 18:56:38 +0200

    czesc razor pages

---
+++ zajecia8/Projekt pierwszy/Projekt pierwszy/Pages/Name.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projekt_pierwszy.Pages
+{
+    public class NameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
